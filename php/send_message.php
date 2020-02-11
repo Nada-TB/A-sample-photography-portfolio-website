82c3be0463a7aec76e9a4be4df210543c5831eeb
Nada <nada.tebba@hotmail.fr>
2020-02-11 23:44:40 +0100

    create variables

--- php/send_message.php
+++ php/send_message.php
@@ -4,16 +4,17 @@ require_once"utilities.php";
 require_once"dataBase.php";
 
 $response=json_decode($_POST['response'], false);
+$name=$response->name;
 $email=$response->email;
+$subject=$response->subject;
+$message=$response->message;
 
 if(!empty($response) && filter_var($email, FILTER_VALIDATE_EMAIL)){
-    
     $contact=$pdo->prepare("INSERT INTO contacts( name, email, subject, message) VALUES (?,?,?,?)");
-    $contact->execute([test_input($response->name),$email, test_input($response->subject), test_input($response->message)]);
-
+    $contact->execute([test_input($name),$email, test_input($subject), test_input($message)]); 
     $response=json_encode("thanks for your mail, I'll respond you as soon as possible");
     echo $response;
-   
+
 }else{
     $response=json_encode("error mail");
     echo $response;
