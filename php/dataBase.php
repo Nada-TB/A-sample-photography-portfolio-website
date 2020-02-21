24c720d6be7d79906a438c2753b1cd6cc7911ca2
Nada <nada.tebba@hotmail.fr>
2020-02-21 18:00:49 +0100

    create class contact

--- php/dataBase.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$pdo= new PDO("mysql:host=localhost;dbname=portfolio_3wc;charset=UTF8", "root", "");
--- php/send_message.php
+++ php/send_message.php
@@ -1,21 +1,18 @@
-<?php
+<?php 
 
-require_once"utilities.php";
-require_once"dataBase.php";
+require_once 'class_dataBase.php';
+require_once 'class_contact_send_message.php';
 
 $response=json_decode($_POST['response'], false);
 $name=$response->name;
 $email=$response->email;
 $subject=$response->subject;
 $message=$response->message;
-
-if(!empty($response) && filter_var($email, FILTER_VALIDATE_EMAIL)){
-    $contact=$pdo->prepare("INSERT INTO contacts( name, email, subject, message) VALUES (?,?,?,?)");
-    $contact->execute([test_input($name),$email, test_input($subject), test_input($message)]); 
-    $response=json_encode("thanks for your mail, I'll respond you as soon as possible");
-    echo $response;
-
+if(!empty($response)){
+    $contact= new contact();
+    $message=$contact->create_contact($name, $email, $subject,$message);
+    echo $message;
 }else{
-    $response=json_encode("error mail");
-    echo $response;
+    $message=json_encode("you must fill all of the fields");
+    echo $message;
 }
